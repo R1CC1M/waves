f7b8960fb1a8c2bc66f52e74cfded650ef06bc2e
Ricci Mwangela <rmwangela@apple.com>
2022-06-03 11:22:22 +0100

    player layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import { ChakraProvider, extendTheme } from '@chakra-ui/react'
 import 'reset-css'
+import PlayerLayout from '../components/playerLayout'
 
 const theme = extendTheme({
   colors: {
@@ -31,8 +32,10 @@ const theme = extendTheme({
 
 function MyApp({ Component, pageProps }) {
   return (
-  <ChakraProvider theme={theme}>
-    <Component {...pageProps} />
+   <ChakraProvider theme={theme}>
+     <PlayerLayout>
+        <Component {...pageProps} />
+     </PlayerLayout>
   </ChakraProvider>
   )
 }
