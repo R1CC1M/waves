b49d7ff59bb8fd3f821cb5c56736b299754c26aa
Ricci Mwangela <rmwangela@apple.com>
2022-06-06 01:02:04 +0100

    added first name and last names to user model

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,15 +2,17 @@ import { Box, Flex, Text } from '@chakra-ui/react'
 import { Image } from '@chakra-ui/image'
 import GradientLayout from '../components/gradientLayout'
 import prisma from '../lib/prisma'
+import { useMe } from '../lib/hooks'
 
 const Home = ({ artists }) => {
+  const { user } = useMe()
   return (
     <GradientLayout
       roundImage="true"
       color="green"
       subtitle="profile"
-      title="Ricci Mwangela"
-      description="15 public playlists"
+      title={`${user?.firstName} ${user?.lastName}`}
+      description={`${user?.playlistCount} public playlists`}
       image="https://tinted-gym-f99.notion.site/image/https%3A%2F%2Fdl.dropboxusercontent.com%2Fs%2Fbgiv0ssz3xpotz9%2Fpeep.png%3Fdl%3D0?table=block&id=33f9771b-0e6f-4a72-832c-69ed2d41f290&spaceId=511cd811-5561-4a61-b550-c4086b4afafb&width=2000&userId=&cache=v2"
     >
       <Box color="white" paddingX="40px">
